a26814bb9a243bd0fe06050e23d147db54ff6bfc
Damien Tschan <damien.tschan1@he-arc.ch>
2023-10-27 12:10:12 +0200

    Implemented create and store methods

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -12,4 +12,37 @@ class QuestionController extends Controller
         $questions = Question::all();
         return view('questions.index', ['questions' => $questions]);
     }
+
+    public function create()
+    {
+        return view('questions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|min:1|max:30',
+            'statement' => 'required|min:1|max:50',
+            'difficulty' => 'required|integer|gte:1|lte:5',
+            'answer' => 'required|min:1|max:50'
+        ]);
+
+        Question::create($request->all());
+
+        return redirect()->route('questions.index')
+            ->with('success','Question created successfully.');
+    }
+
+    public function show()
+    {
+        return redirect()->route('questions.index')
+            ->with('success','Redirected from "show" route.');
+        //TODO Implémenter
+    }
+
+    public function destroy($id)
+    {
+        Question::destroy($id);
+        //TODO Implémenter
+    }
 }
