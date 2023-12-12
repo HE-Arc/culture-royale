c4c4ab73c61e3374eed32059fada0b0d574d8110
Damien Tschan <damien.tschan1@he-arc.ch>
2023-12-12 10:51:36 +0100

    Scoreboard fully working

---
+++ app/Http/Controllers/ScoreController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Score;
+use Illuminate\Http\Request;
+
+class ScoreController extends Controller
+{
+    public function index(){
+        $scoreboard = Score::orderBy('score', 'DESC')->limit(100)->get();
+        return view('scores.index', ['scores' => $scoreboard]);
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'playername' => 'required|min:1|max:30',
+            'score' => 'required|integer|gte:0|lte:2000000',
+        ]);
+
+        Score::create($request->all());
+
+        return redirect()->route('scores.index')
+            ->with('success', 'Your score has been added to the global scoreboard !');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ModifQController;
+use App\Http\Controllers\ScoreController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::resource('questions', QuestionController::class);
+Route::get('/scores', [ScoreController::class, 'index'])->name("scores.index");
+Route::post('/scores/store', [ScoreController::class, 'store'])->name("scores.store");
