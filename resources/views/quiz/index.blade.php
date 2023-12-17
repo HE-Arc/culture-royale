de067fd991b45038087d82c0b59cddcc18057710
Jonas Flückiger <fluckiger.jonas.dev@gmail.com>
2023-12-17 15:37:21 +0100

    Removed card layout

--- resources/views/quiz/index.blade.php
+++ resources/views/quiz/index.blade.php
@@ -1,54 +1,43 @@
 @extends('layout.app')
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-<link rel="stylesheet" href="{{asset('css/layout/game.css')}}">
+<link rel="stylesheet" href="{{ asset('css/layout/game.css') }}">
 @section('content')
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-md-8 offset-md-2">
-            <div class="card">
-                <div class="card-header">
-                    <strong>Question :</strong>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <strong>Question :</strong>
+                <p id="question">{{ $question->statement }}</p>
+                @if ($question->image)
+                    <img id="question-image" src="{{ $question->image ? asset('images/' . $question->image) : '' }}"
+                        alt="Question Image" class="img-fluid mb-3" style="{{ $question->image ? '' : 'display: none;' }}">
+                @endif
+                <div id="timer" class="alert alert-warning text-center">
+                    Temps : <strong>10 secondes</strong>
                 </div>
-                <div class="card-body">
-                    <p id="question">{{ $question->statement }}</p>
-                    @if ($question->image)
-                    <img id="question-image" src="{{ $question->image ? asset('images/' . $question->image) : '' }}" alt="Question Image" class="img-fluid mb-3" style="{{ $question->image ? '' : 'display: none;' }}">
-                    @endif
-                    <div id="timer" class="alert alert-warning text-center">
-                        Temps : <strong>10 secondes</strong>
+
+                <!-- Answer Form -->
+                <form id="answer-form">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="answer" placeholder="Enter your answer">
                     </div>
-
-                    <!-- Answer Form -->
-                    <form id="answer-form">
-                        <div class="form-group">
-                            <input type="text" class="form-control" id="answer" placeholder="Enter your answer">
-                        </div>
-                        <button type="submit" class="btn btn-primary">Répondre</button>
-                    </form>
-                    <div id="score">Score: 0</div>
-                    <div id="end-message"></div>
-
-
-                </div>
+                    <button type="submit" class="btn btn-success">Répondre</button>
+                </form>
+                <div id="score">Score: 0</div>
+                <div id="end-message"></div>
             </div>
         </div>
     </div>
-</div>
+    </div>
 
-<script src="{{ asset('storage/quiz.js') }}"></script>
-<script>
-    document.addEventListener('DOMContentLoaded', function() { //au cas ou le contenu charge avant le script
-        initializeQuiz({
-            submitUrl: '{{ route('quiz.submit') }}',
-            questionId: '{{ $question->id }}',
-            nextQuestionUrl: '{{ route('quiz.index') }}',
-            image_url: '{{ $question->image }}',
+    <script src="{{ asset('storage/quiz.js') }}"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() { //au cas ou le contenu charge avant le script
+            initializeQuiz({
+                submitUrl: '{{ route('quiz.submit') }}',
+                questionId: '{{ $question->id }}',
+                nextQuestionUrl: '{{ route('quiz.index') }}',
+                image_url: '{{ $question->image }}',
+            });
         });
-    });
-</script>
-
-
-
-
+    </script>
 @endsection
-
